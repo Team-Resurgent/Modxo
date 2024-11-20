b2eca5e37c2b0255c32547bc2bf330970db8697c
EqUiNoX <equinox.teamresurgent@gmail.com>
2024-11-19 21:08:16 -0700

    i2c changes

--- src/modxo/legacy_display/legacy_display.h
+++ src/modxo/legacy_display/legacy_display.h
@@ -7,4 +7,6 @@ void legacy_display_data(uint8_t *data);
 void legacy_display_poll();
 void legacy_display_set_spi();
 void legacy_display_set_i2c(uint8_t i2c_address);
+void legacy_display_remove_i2c_prefix();
+void legacy_display_set_i2c_prefix(uint8_t prefix);
 void legacy_display_init();
